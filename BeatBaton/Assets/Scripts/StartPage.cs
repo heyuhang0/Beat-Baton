5fe16a40072c186518dcc25620e9242786c8a254
heyuhang0 <heyuhang0@outlook.com>
2018-11-12 00:24:31 +0800

    Add an IP indicator to start page

--- BeatBaton/Assets/Scripts/StartPage.cs
+++ BeatBaton/Assets/Scripts/StartPage.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 
 public class StartPage : MonoBehaviour {
+	public GameObject listContentItemTemplate;
+	public Transform parentTransform;
+	public GameObject contentView;
 	// Use this for initialization
 	void Start () {
+		string[] ips = LocalIPAddresses();
+		foreach (string ip in ips) {
+			Debug.Log(ip);
+		}
+
+		listContentItemTemplate.SetActive(false);
+
+		contentView.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 20 * ips.Length);
+		foreach (string ip in ips) {
+			GameObject newOne = Instantiate(listContentItemTemplate, parentTransform);
+			Text text = newOne.GetComponentInChildren<Text>();
+			text.text = ip;
+			newOne.SetActive(true);
+		}
 		
 	}
 	
@@ -28,4 +47,19 @@ public class StartPage : MonoBehaviour {
 	public void onButtonExit () {
 		Application.Quit();
 	}
+
+	private string[] LocalIPAddresses()
+     {
+         IPHostEntry host;
+         host = Dns.GetHostEntry(Dns.GetHostName());
+		 ArrayList ipList = new ArrayList();
+         foreach (IPAddress ip in host.AddressList)
+         {
+             if (ip.AddressFamily == AddressFamily.InterNetwork)
+             {
+				 ipList.Add(ip.ToString());
+             }
+         }
+         return (string[])ipList.ToArray(typeof(string));
+     }
 }
